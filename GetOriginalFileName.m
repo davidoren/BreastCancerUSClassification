13292473dc037472801920371faf7a3730a16e4f
davidoren <davidoren26@gmail.com>
2014-11-24 16:32:25 +0200

    delete convex and improved new main

--- GetOriginalFileName.m
+++ GetOriginalFileName.m
@@ -1,4 +1,11 @@
-function orig_path = GetOriginalFileName(seg_path)
-    ind = strfind(seg_path, '_1');
-    orig_path = strcat(seg_path(1 : ind - 1), seg_path(ind + 2 : end));
+function orig_path = GetOriginalFileName(seg_path, orig_dir)
+    if strcmp(orig_dir(end), '\\')
+        dirname = orig_dir(1 : end - 1);
+    else
+        dirname = orig_dir(1 : end);
+    end
+    filename = strsplit(seg_path, '\\');
+    filename = filename{end};
+    ind = strfind(filename, '_1');
+    orig_path = [dirname '\\' strcat(filename(1 : ind - 1), filename(ind + 2 : end))];
 end
