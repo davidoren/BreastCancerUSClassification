08970281e659e1080913914d2f3f57270a9e8132
davidoren <davidoren26@gmail.com>
2014-12-16 15:35:37 +0200

    NewMain - results as cell array
    still needs fix.

--- NewMain.m
+++ NewMain.m
@@ -13,10 +13,30 @@ lesiontypes = cellfun('isempty',strfind(filenames,'.0'));
 
 N = size(filenames, 2);
 current_image = LesionImg([BmodePath filenames{1}], [cd BmodeOrigPath]);
-Results = zeros(size(lesiontypes, 2), size(current_image.get_features, 2) + 2);
+num_of_features = size(current_image.get_features, 2);
+Results = zeros(size(lesiontypes, 2), num_of_features + 2);
 for i = 1:N
     current_image = LesionImg([BmodePath filenames{i}], [cd BmodeOrigPath]);
     Results(i, 2 : end - 1) = current_image.get_features;
     Results(i, 1) = str2num(filenames{i}(1 : strfind(filenames{i}, '.') - 1));
     Results(i, end) = lesiontypes(i);
 end
+%%
+% Construct a cell array with the titles and all results.
+Output = [{'Image No.', current_image.titles, 'Class'}; mat2cell(Results, ones(1, N), ones(1, num_of_features))];
+
+% Replacing all features that could not be extracted with a question mark.
+for i = 1:numel(Output)
+    if Output{i} == -Inf
+        Output{i} = '?';
+    end
+end
+
+for i = 1:N
+    if Output(i, end) == 0
+        Output(i, end) = 'Benign';
+    else
+        Output(i, end) = 'Malignant';
+    end
+end
+%%
